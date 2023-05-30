ddbb2abb99053f1bfb1c77b82cb37d6036b67182
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-05-30 19:13:32 -0300

    Alterações

--- gerador/gerador/settings.py
+++ gerador/gerador/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!zx&^=78=#ae5w@oi3j#mf#*e^dt(jmhww9m3q#s!^dr(+)e*h'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
